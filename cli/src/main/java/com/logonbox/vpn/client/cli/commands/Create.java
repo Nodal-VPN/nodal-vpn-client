454d31b7b14d3d0ef4a46eb277e69b7405077350
Lee Painter <lee@ludup.com>
2024-04-24 11:58:16 +0100

    Revert "Test to see if this fixes CLI create on command line issue."
    This reverts commit b547efce338fe35ad85563b867a36b110595dbf0.

--- cli/src/main/java/com/logonbox/vpn/client/cli/commands/Create.java
+++ cli/src/main/java/com/logonbox/vpn/client/cli/commands/Create.java
@@ -48,7 +48,6 @@ public class Create extends AbstractConnectionCommand implements Callable<Intege
 
 		var cli = getCLI();
 		var vpnManager = cli.getVpnManager();
-		vpnManager.start();
         var connectionId = vpnManager.getVpnOrFail().getConnectionIdForURI(uriObj.toASCIIString());
 		var console = cli.getConsole();
 		var err = console.err();
