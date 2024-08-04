31c47428f1fa85a5cc9874729060277d361c37da
Brett Smith <brett@logonbox.com>
2024-08-04 13:39:39 +0100

    Tray icon app name tweaks

--- desktop/src/main/java/com/logonbox/vpn/client/desktop/DesktopVPNApp.java
+++ desktop/src/main/java/com/logonbox/vpn/client/desktop/DesktopVPNApp.java
@@ -487,9 +487,9 @@ public class DesktopVPNApp extends JajaFXApp<DesktopVPN, DesktopVPNAppWindow> im
                     LOG.info("Starting tray");
                     ProcessBuilder bldr;
                     if (OS.isWindows())
-                        bldr = new ProcessBuilder(System.getProperty("user.dir") + File.separator + "jad-vpn-tray-icon.exe");
+                        bldr = new ProcessBuilder(System.getProperty("user.dir") + File.separator + "jad-vpn-tray.exe");
                     else
-                        bldr = new ProcessBuilder(System.getProperty("user.dir") + File.separator + "jad-vpn-tray-icon");
+                        bldr = new ProcessBuilder(System.getProperty("user.dir") + File.separator + "jad-vpn-tray");
                     bldr.redirectError(Redirect.INHERIT);
                     bldr.redirectOutput(Redirect.INHERIT);
                     bldr.start();
