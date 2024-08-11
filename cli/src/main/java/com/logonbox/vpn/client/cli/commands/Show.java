2278ab0ffd9203163b5097d642e553b2419f7fc9
Brett Smith <brett@logonbox.com>
2024-08-11 18:55:58 +0100

    ANSI styles ans internationalised the show command.

--- cli/src/main/java/com/logonbox/vpn/client/cli/commands/Show.java
+++ cli/src/main/java/com/logonbox/vpn/client/cli/commands/Show.java
@@ -1,11 +1,14 @@
 package com.logonbox.vpn.client.cli.commands;
 
+import com.logonbox.vpn.client.cli.CLI;
 import com.logonbox.vpn.client.common.Utils;
 import com.logonbox.vpn.client.common.api.IVpnConnection;
 
 import java.io.PrintWriter;
 
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Help.Ansi;
 import picocli.CommandLine.Parameters;
 
 @Command(name = "show", aliases = { "s", "sh", "view" }, usageHelpAutoWidth = true, mixinStandardHelpOptions = true, description = "Show connection details.")
@@ -13,6 +16,9 @@ public class Show extends AbstractConnectionCommand {
 
 	@Parameters(description = "Show connection details.")
 	private String[] names;
+
+    @Option(names = { "-s", "--scripts" }, description = "Show contents of scripts as well.")
+    private boolean showScripts;
 
 	@Override
 	public Integer call() throws Exception {
@@ -30,39 +36,51 @@ public class Show extends AbstractConnectionCommand {
 	}
 
 	private void printConnection(PrintWriter writer, IVpnConnection connection) {
-		writer.println(String.format("Basic"));
-		writer.println(String.format(" Connection: %d", connection.getId()));
-		writer.println(String.format(" Name: %s", connection.getName()));
-		writer.println(String.format(" Uri: %s", connection.getUri(true)));
-		writer.println(String.format(" Status: %s", connection.getStatus()));
-		writer.println(String.format(" Mode: %s", connection.getMode()));
-		writer.println(String.format(" Transient: %s", connection.isTransient() ? "Yes" : "No"));
-		writer.println(String.format(" Authorized: %s", connection.isAuthorized() ? "Yes" : "No"));
-		writer.println(String.format(" Shared: %s", connection.isShared() ? "Yes" : "No"));
-		writer.println(String.format(" Connect At Startup: %s", connection.isConnectAtStartup() ? "Yes" : "No"));
-		writer.println(String.format(" Stay Connected: %s", connection.isStayConnected() ? "Yes" : "No"));
-		writer.println(String.format(" Owner: %s", connection.getOwner()));
+		writer.println(Ansi.AUTO.string("@|underline " + CLI.BUNDLE.getString("show.basic") + "|@"));
+		writeDetail(writer, "connection", String.valueOf(connection.getId()));
+        writeDetail(writer, "name", connection.getDisplayName());
+        writeDetail(writer, "uri", connection.getUri(true));
+        writeDetail(writer, "status", CLI.BUNDLE.getString("status." + connection.getStatus()));
+        writeDetail(writer, "mode", connection.getMode());
+        writeDetail(writer, "transient", connection.isTransient() ? CLI.BUNDLE.getString("show.yes") : CLI.BUNDLE.getString("show.no"));
+        writeDetail(writer, "authorized", connection.isAuthorized() ? CLI.BUNDLE.getString("show.yes") : CLI.BUNDLE.getString("show.no"));
+        writeDetail(writer, "shared", connection.isShared() ? CLI.BUNDLE.getString("show.yes") : CLI.BUNDLE.getString("show.no"));
+        writeDetail(writer, "connectAtStartup", connection.isConnectAtStartup() ? CLI.BUNDLE.getString("show.yes") : CLI.BUNDLE.getString("show.no"));
+        writeDetail(writer, "stayConnected", connection.isStayConnected() ? CLI.BUNDLE.getString("show.yes") : CLI.BUNDLE.getString("show.no"));
+        writeDetail(writer, "owner", connection.getOwner());
 		if(connection.isAuthorized()) {
-			writer.println(String.format("VPN"));
-			writer.println(String.format(" Interface: %s", connection.getInterfaceName()));
-			writer.println(String.format(" Address: %s", connection.getAddress()));
-			writer.println(String.format(" Endpoint: %s:%d", connection.getEndpointAddress(), connection.getEndpointPort()));
-			writer.println(String.format(" DNS: %s", String.join(",", connection.getDns())));
+	        writer.println(Ansi.AUTO.string("@|underline " + CLI.BUNDLE.getString("show.vpn") + "|@"));
+	        writeDetail(writer, "interface", connection.getInterfaceName());
+            writeDetail(writer, "address", connection.getAddress());
+            writeDetail(writer, "endpoint", String.format("%s:%d", connection.getEndpointAddress(), connection.getEndpointPort()));
+            writeDetail(writer, "dns", String.join(",", connection.getDns()));
 			if(!connection.isRouteAll())
-				writer.println(String.format(" Allowed IPs: %s", String.join(",", connection.getAllowedIps())));
-			writer.println(String.format(" Route All: %s", connection.isRouteAll() ? "Yes" : "No"));
-			writer.println(String.format(" User Public Key: %s", connection.getUserPublicKey()));
-			writer.println(String.format(" Server Public Key: %s", connection.getPublicKey()));
-			writer.println(String.format(" MTU: %s", connection.getMtu()));
-			writer.println(String.format(" Persistent Keep Alive: %s", connection.getPersistentKeepalive()));
-			if(Utils.isNotBlank(connection.getPreUp()))
-				writer.println(String.format(" Pre-Up: %s", connection.getPreUp()));
-			if(Utils.isNotBlank(connection.getPostUp()))
-				writer.println(String.format(" Post-Up: %s", connection.getPostUp()));
-			if(Utils.isNotBlank(connection.getPreDown()))
-				writer.println(String.format(" Pre-Down: %s", connection.getPreDown()));
-			if(Utils.isNotBlank(connection.getPostDown()))
-				writer.println(String.format(" Post-Down: %s", connection.getPostDown()));
+	            writeDetail(writer, "allowedIps", String.join(",", connection.getAllowedIps()));
+	        writeDetail(writer, "routeAll", connection.isRouteAll() ? CLI.BUNDLE.getString("show.yes") : CLI.BUNDLE.getString("show.no"));
+            writeDetail(writer, "userPublicKey", connection.getUserPublicKey());
+            writeDetail(writer, "serverPublicKey", connection.getPublicKey());
+            writeDetail(writer, "mtu", String.valueOf(connection.getMtu()));
+            writeDetail(writer, "persistentKeepalive", String.valueOf(connection.getPersistentKeepalive()));
+            if(showScripts) {
+    			if(Utils.isNotBlank(connection.getPreUp()))
+    	            writeDetail(writer, "preUp", connection.getPreUp());
+    			if(Utils.isNotBlank(connection.getPostUp()))
+                    writeDetail(writer, "postUp", connection.getPostUp());
+    			if(Utils.isNotBlank(connection.getPreDown()))
+                    writeDetail(writer, "preDown", connection.getPreDown());
+    			if(Utils.isNotBlank(connection.getPostDown()))
+                    writeDetail(writer, "postDown", connection.getPostDown());
+            }
+            else {
+                writeDetail(writer, "scripts", Utils.isNotBlank(connection.getPreUp()) ||
+                        Utils.isNotBlank(connection.getPreDown()) ||
+                        Utils.isNotBlank(connection.getPostUp()) ||
+                        Utils.isNotBlank(connection.getPostDown()) ? CLI.BUNDLE.getString("show.yes") : CLI.BUNDLE.getString("show.no"));
+            }
 		}
 	}
+
+	private void writeDetail(PrintWriter writer, String key,  String val) {
+        writer.println(Ansi.AUTO.string(String.format(" %-22s @|faint :|@ @|bold %s|@", CLI.BUNDLE.getString("show." + key), val)));
+    }
 }
